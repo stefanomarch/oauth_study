406d88713acfeb2d24c2f1055a6018d1f5774a5d
stefanomarch <stefano.marchetti20@gmail.com>
2022-11-01 20:26:28 +0100

    set up omniauthable for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,18 @@
 # frozen_string_literal: true
 
+# User model
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: %i[facebook github google_oauth2]
+
+  # method to find the user by provider_data , if not found it retrieve email from provider_data and feed devise with a friendly password because
+  # devise needs the password
+  def self.create_from_provider_data(provider_data)
+    where(provider: provider_data.provider, uid: provider_data.uid).first_or_create do |user|
+      user.email = provider_data.info.email
+      user.password = Devise.friendly_token[0, 20]
+    end
+  end
 end
