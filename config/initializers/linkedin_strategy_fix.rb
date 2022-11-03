016c4b2878c4cf1b6a3e7ed755489d9524b7c432
stefanomarch <stefano.marchetti20@gmail.com>
2022-11-03 18:44:52 +0100

    added top level documentation for linkedin gem

--- config/initializers/linkedin_strategy_fix.rb
+++ config/initializers/linkedin_strategy_fix.rb
@@ -2,6 +2,7 @@
 
 module OmniAuth
   module Strategies
+    # likedin provider couldn t read the id_secret ,so added manually the client_secret to build the token 🥰
     class LinkedIn < OmniAuth::Strategies::OAuth2
       def token_params
         super.tap do |params|
